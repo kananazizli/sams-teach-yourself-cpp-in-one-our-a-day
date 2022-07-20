33d90c5d3eb97d0a46b0522f9b6fbd6dfde3f20e
kananazizli <kananazizli.ao@gmail.com>
2022-07-20 05:29:05 -0400

    Added a solution to Exercise 3 for Chapter 2 - The Anatomy of a C++ Program

---
+++ chapter_2_the_anatomy_of_a_cpp_program/exercise_3/exercise_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Declare a function
+int DemoConsoleOutput();
+
+int main()
+{
+	// Call i.e. invoke the function
+	DemoConsoleOutput();
+
+	return 0;
+}
+
+// Define i.e. implement the previously declared function
+int DemoConsoleOutput()
+{
+	cout << "This is a simple example of subtraction: 15 - 10 = " << 15 - 10 << endl;
+	cout << "And this is a simple example of multiplication: 10 * 5 = " << 10 * 5 << endl;
+
+	return 0;
+}  
