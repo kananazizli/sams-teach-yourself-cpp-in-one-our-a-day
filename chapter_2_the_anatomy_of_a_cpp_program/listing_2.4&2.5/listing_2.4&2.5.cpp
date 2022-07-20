c16e99b706b08aae932d374e7f8f262922545b28
kananazizli <kananazizli.ao@gmail.com>
2022-07-20 04:24:48 -0400

    Added Listing 2.5 "Using the Return Value of a Function" to code file listing_2.4&2.5

--- chapter_2_the_anatomy_of_a_cpp_program/listing_2.4&2.5/listing_2.4&2.5.cpp
+++ chapter_2_the_anatomy_of_a_cpp_program/listing_2.4&2.5/listing_2.4&2.5.cpp
@@ -1,3 +1,5 @@
+/*
+
 // Declaring, Defining, and Calling a Function That Demonstrates Capabilities of std::cout
 
 #include <iostream>
@@ -23,4 +25,27 @@ int DemoConsoleOutput()
 	cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
 
 	return 0;
-} 
+}  
+
+*/
+
+// Using the Return Value of a Function
+#include <iostream>
+using namespace std;
+
+// Function declaration and definition
+int DemoConsoleOutput()
+{
+	cout << "This is a simple string literal" << endl;
+	cout << "Writing number five: " << 5 << endl;
+	cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+	cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+
+	return 0;
+}
+
+int main()
+{
+	// Function call with return used to exit
+	return DemoConsoleOutput();
+}
