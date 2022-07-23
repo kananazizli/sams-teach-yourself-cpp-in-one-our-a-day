34448e62b205bf39c8b726bda8c0442582a1bc58
kananazizli <kananazizli.ao@gmail.com>
2022-07-23 08:12:42 -0400

    Added Listing 3.8 "Using consteval and constexpr to Calculate Pi and Multiples of Pi"

---
+++ chapter_3_using_variables_declaring_constants/listing_3.8/listing_3.8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+consteval double GetPi() {return 22.0 / 7;}
+constexpr double XPi(int x) {return x * GetPi();}
+
+int main()
+{
+	using namespace std;
+	constexpr double pi = GetPi();
+	
+	cout << "constexpr pi evaluated by compiler to " << pi << endl;
+	cout << "constexpr XPi(2) evaluated by compiler to " << XPi(2) << endl;
+	
+	int multiple = 5;
+	cout << "(non-const) integer multiple = " << multiple << endl;
+	cout << "constexpr is ignored when XPi(multiple) is invoked, ";
+	cout << "return " << XPi(multiple) << endl;
+	
+	
+	return 0;
+}
