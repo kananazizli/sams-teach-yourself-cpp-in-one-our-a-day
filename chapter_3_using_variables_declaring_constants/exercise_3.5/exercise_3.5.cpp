c6e8046613271602f16667b0afaf3ef4ccd8932b
kananazizli <kananazizli.ao@gmail.com>
2022-07-24 05:26:07 -0400

    Added a solution to Exercise 5 for Chapter 3 - Using Variables, Declaring Constants

---
+++ chapter_3_using_variables_declaring_constants/exercise_3.5/exercise_3.5.cpp
@@ -0,0 +1,11 @@
+/* What is wrong in the following initialization?
+
+auto age;
+
+This code will fail the compilation stage because it does not contain an initialization.
+
+The correct version would be: 
+
+*/
+
+auto age = 18;
